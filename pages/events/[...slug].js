99c92b82462dc4a3a3c1a26178d9c526f6296ea1
litotechenterprise <litotechenterprise@gmail.com>
2022-04-22 16:50:17 -0400

    initial commit

---
+++ pages/events/[...slug].js
@@ -0,0 +1,10 @@
+import { useRouter } from "next/router"
+
+
+export default function EventSlugs () {
+    return (
+        <div>
+            <h1>Events Slugs</h1>
+        </div>
+    )
+}
---
+++ pages/events/[id].js
@@ -0,0 +1,9 @@
+import { useRouter } from "next/router"
+
+export default function EventId () {
+    return (
+        <div>
+            <h1>Events id</h1>
+        </div>
+    )
+}
